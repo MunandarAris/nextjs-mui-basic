e3315e1fc94ef85e6d833ae48d9550903278f555
Aris Munandar <arismun0010@gmail.com>
2022-02-06 15:17:37 +0700

    v1

--- config/theme.js
+++ config/theme.js
@@ -2,7 +2,20 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    mode: "light",
+    primary: {
+      main: "#FC881D",
+    },
+  },
+  typography: {
+    fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+    },
   },
 });
 
